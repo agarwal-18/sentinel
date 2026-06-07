4f360b97a32a667f05dd95821d72ebd16d6311bc
Aditya Agarwal <aaagrawal098@gmail.com>
2026-06-07 16:30:39 +0530

    feat: add auth routes

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const express = require('express');
-const app = use.express();
-    
+require('dotenv').config();
+const pool = require('./src/db/index');
+const auth = require('./src/routes/auth');
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({ connected: true, time: result.rows[0].now });
+    }
+    catch (err) {
+        res.status(500).json({connected: false, error: err.message});
+        console.log(err);
+    }
+})
+
+app.use('/auth', auth);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Sentinel running on port ${process.env.PORT}`)
+})
---
+++ src/controllers/auth.js
@@ -0,0 +1,58 @@
+const pool = require('../db/index')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+async function findUserByEmail(email) {
+    return await pool.query('SELECT id, email, password_hash FROM users WHERE email = $1', [email]);
+}
+
+async function register(req, res) {
+    try {
+        const { name, email, password } = req.body;
+        const userRecord = await findUserByEmail(email);
+
+        if (userRecord.rowCount > 0) {
+            return res.status(400).json({error: 'Email is already in use.'});
+        }
+            
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query('INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3)', [name, email, hashedPassword]); 
+        return res.status(201).json({message: 'User Registered Successfully.'});
+    }
+    catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+    
+        const userRecord = await findUserByEmail(email);
+
+        if (userRecord.rowCount <= 0) {
+            return res.status(400).json({error: 'Email not registered.'});
+        }
+            
+        const storedHash = userRecord.rows[0].password_hash;
+        const passwordMatch = await bcrypt.compare(password, storedHash);
+
+        if (passwordMatch) {
+            const token = jwt.sign(
+                { userId: userRecord.rows[0].id, email: email },
+                process.env.JWT_SECRET,
+                { expiresIn: '7d'}
+            );
+            return res.status(200).json({message: 'Login Successful.', token});
+        }
+            
+        return res.status(400).json({error: 'Incorrect Password.'});
+    }
+    catch (err) {
+        res.status(500).json({error: err.message});
+    }
+}
+
+
+module.exports = { register, login };
