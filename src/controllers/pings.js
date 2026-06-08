a9a4e8d112ced7c443f74f45ac8dfe1f5625e872
Aditya Agarwal <aaagrawal098@gmail.com>
2026-06-08 23:19:47 +0530

    feat: add ping and uptime route

---
+++ src/controllers/pings.js
@@ -0,0 +1,59 @@
+const pool = require('../db/index');
+
+async function verifyMonitor(monitorId, userId) {
+    
+    const monitor = await pool.query(`
+        SELECT id FROM monitors
+        WHERE id = $1 AND user_id = $2`,
+        [monitorId, userId]
+    );
+    return monitor.rowCount;
+}
+
+async function getPings(req, res) {
+    try {
+        const monitor = await verifyMonitor(req.params.id, req.user.userId);
+        if (monitor === 0) return res.status(404).json({ error: 'Monitor not found' });
+        
+        const pingRecord = await pool.query(`
+            SELECT * FROM pings WHERE monitor_id = $1`,
+            [req.params.id]
+        );
+
+        if (pingRecord.rowCount === 0) return res.status(200).json({pings: []});
+        return res.status(200).json({pings: pingRecord.rows});
+    }
+    catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+}
+
+async function getUptime(req, res) {
+    try {
+        const monitor = await verifyMonitor(req.params.id, req.user.userId);
+        if (monitor === 0) return res.status(404).json({ error: 'Monitor not found.' });
+        
+        const result = await pool.query(`
+            SELECT 
+            COUNT(*) AS total,
+            COUNT(*) FILTER (WHERE is_up = true) AS up_count 
+            FROM pings
+            WHERE monitor_id = $1 
+            AND checked_at >= NOW() - INTERVAL '90 days'`,
+            [req.params.id]
+        );
+
+        const { total, up_count } = result.rows[0];
+
+        if (parseInt(total) === 0) return res.status(200).json({uptime: null});
+
+        const uptimePercent = ( parseInt(up_count) / parseInt(total) ) * 100;
+        return res.status(200).json({uptime: uptimePercent.toFixed(2)});
+    }
+
+    catch (err) {
+        return res.status(500).json({error: err.message});
+    }
+}
+
+module.exports = { getPings, getUptime };
--- src/routes/monitors.js
+++ src/routes/monitors.js
@@ -9,9 +9,17 @@ const {
     deleteMonitor
 } = require('../controllers/monitors');
 
+const {
+    getPings,
+    getUptime
+} = require('../controllers/pings');
+
 router.get('/', validateToken, getMonitors);
 router.get('/:id', validateToken, getMonitorById);
 router.post('/', validateToken, createMonitor);
 router.delete('/:id', validateToken, deleteMonitor);
+
+router.get('/:id/pings', validateToken, getPings);
+router.get('/:id/uptime', validateToken, getUptime);
 
 module.exports = router;
