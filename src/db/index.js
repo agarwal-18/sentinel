9f6c72b755a77c9ca682e2790faa319494054e69
Aditya Agarwal <aaagrawal098@gmail.com>
2026-06-25 01:25:18 +0530

    chore: remove database connection status logging

--- src/db/index.js
+++ src/db/index.js
@@ -4,14 +4,4 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
-
-pool.connect((err, client, release) => {
-    if (err) {
-        console.error('DATABASE CONNECTION FAILED:', err.message)
-    } else {
-        console.log('DATABASE CONNECTED SUCCESSFULLY')
-        release()
-    }
-})
-
 module.exports = pool;
